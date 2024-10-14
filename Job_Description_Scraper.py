e81a8f6cbf24317b9268580d20a113ad408cecd9
schavan1990 <sumitmbacanada@gmail.com>
2024-10-14 01:09:40 -0400

    Added sample URls to test

--- Job_Description_Scraper.py
+++ Job_Description_Scraper.py
@@ -22,3 +22,9 @@ def fetch_job_description(url):
 
     except requests.exceptions.RequestException as e:
         return f"An error occurred while fetching the job description: {str(e)}"
+
+# URLs to use for testing
+
+# https://www.onespan.com/about/careers/job/senior-product-manager-6320662
+# https://www.onespan.com/about/careers/job/junior-production-engineer-6239056
+# https://www.vortex-intl.com/jobs/product-manager/
